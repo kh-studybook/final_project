513883edabaaf2dfe3c9c7fc038caffd16f261fa
user1 <user1@DESKTOP-8UC660Q>
2020-01-07 15:53:06 +0900

    //

--- studybook/src/main/java/kh/finalproject/studybook/controller/MemberController.java
+++ studybook/src/main/java/kh/finalproject/studybook/controller/MemberController.java
@@ -1,5 +1,13 @@
 package kh.finalproject.studybook.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
 
+	@RequestMapping(value="/room_detail")
+	public String room_detail() {
+		return "room/room_detail";
+	}
 }
