cbf597b627931289054a46d821c4100db1114e24
user1 <user1@DESKTOP-8UC660Q>
2020-01-10 10:26:05 +0900

    java파일 한글깨져서 수정

--- studybook/src/main/java/kh/finalproject/studybook/controller/RoomController.java
+++ studybook/src/main/java/kh/finalproject/studybook/controller/RoomController.java
@@ -16,45 +16,34 @@ import kh.finalproject.studybook.service.RoomService;
 public class RoomController {
 	@Autowired
 	private RoomService roomservice;
-	
-	
-		//지은 추가 //어드민으로 접속 , 룸리스트 보여줌//
-		@RequestMapping(value="/admin", method=RequestMethod.GET)//추후 모델앤뷰로 바꿔야함
-		public String ADaccess() {
-			return "admin/admin_index";
-		}
-		//어드민 룸등록 이동
-		@GetMapping(value = "/RoomWrite.ro")
-		public String board_write() throws Exception {
-			return "admin/room_register";
-		}
-		
 
-	
-	 //어드민 룸등록 액션- 다중업로드 시작
-	 
-	 @GetMapping("/RoomAddAction.ro") public String room_write_ok(Room room,
-	HttpServletRequest request) throws Exception{
-	
-		 
-		 return "redirect:RoomList.ro";
-	 }
-	
-		
+	// 지은 시작--
+	// 어드민으로 접속 , 룸리스트 보여줌
+	@RequestMapping(value = "/admin", method = RequestMethod.GET) // 異뷀썑 紐⑤뜽�븻酉곕줈 諛붽퓭�빞�븿
+	public String ADaccess() {
+		return "admin/admin_index";
+	}
 
-	/*
-	 * //어드민 룸등록 액션
-	 * 
-	 * @PostMapping("/RoomAddAction.ro") public String room_write_ok(Room room,
-	 * HttpServletRequest request) throws Exception{
-	 * 
-	 * }
-	 */
-		//룸 정보 보기 -연습용(은지)
-		@RequestMapping(value="/room_detail")
-		public String room_detail() {
-			return "room/detail";
-		}
+	// 어드민 룸등록 이동
+	@GetMapping(value = "/RoomWrite.ro")
+	public String board_write() throws Exception {
+		return "admin/room_register";
+	}
 
-	
+	// 어드민 룸등록 액션- 다중업로드 시작
+
+	@GetMapping("/RoomAddAction.ro")
+	public String room_write_ok(Room room, HttpServletRequest request) throws Exception {
+
+		return "redirect:RoomList.ro";
+	}
+
+	// 지은 끝--
+
+	// 룸 정보 보기 -연습용(은지)
+	@RequestMapping(value = "/room_detail")
+	public String room_detail() {
+		return "room/detail";
+	}
+
 }
